31885f3fd56a166bb87422c5bf57fc37f2e0323a
Tom Halpin <tom.halpin@dxc.com>
2020-06-28 14:18:41 +0100

    Updates

---
+++ precache-manifest.feaeacba8f77c5037c5b47d0cb656da3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d209369f2484df99d5768cdfd543977",
+    "url": "/react-hello-world/index.html"
+  },
+  {
+    "revision": "a91664f3556327ce6ba4",
+    "url": "/react-hello-world/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39fc79dfefdbeae3f743",
+    "url": "/react-hello-world/static/js/2.0cd8d65c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hello-world/static/js/2.0cd8d65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91664f3556327ce6ba4",
+    "url": "/react-hello-world/static/js/main.6a165cb5.chunk.js"
+  },
+  {
+    "revision": "fcc5dc022e05a67eb594",
+    "url": "/react-hello-world/static/js/runtime-main.09c288aa.js"
+  }
+]);
